090d3ddb205822bb18877cb5e40b44190cd612d6
Naveenaidu <naveennaidu479@gmail.com>
2019-07-20 16:31:30 +0530

    NlCore: Change Parser_lang_comb values to set

--- coalib/nestedlib/NlCore.py
+++ coalib/nestedlib/NlCore.py
@@ -8,7 +8,8 @@ import os
 from importlib import import_module
 
 # The supported Parser for the language combination
-PARSER_LANG_COMB = [{'PyJinjaParser':{"python", "jinja2"}}]
+PARSER_LANG_COMB = [{'PyJinjaParser': {'python', 'jinja2'}}]
+
 
 def get_parser(lang_comb):
     """
@@ -18,14 +19,13 @@ def get_parser(lang_comb):
     parser_name = ''
 
     for parser_lang_comb in PARSER_LANG_COMB:
-        for parser,language in parser_lang_comb.items():
+        for parser, language in parser_lang_comb.items():
             if language == lang_comb:
                 parser_name = parser
 
-    parser_module_string = ('coalib.nestedlib.parsers.' + parser_name )
+    parser_module_string = ('coalib.nestedlib.parsers.' + parser_name)
     parser = getattr(import_module(parser_module_string), parser_name)
     return parser()
-
 
 
 def get_nl_coala_sections(args):
@@ -36,9 +36,9 @@ def get_nl_coala_sections(args):
     nl_sections = OrderedDict()
     for args in arg_list:
         temp_file_name = args.__dict__['files']
-        nl_section_name = "cli_nl_section: " + temp_file_name
+        nl_section_name = 'cli_nl_section: ' + temp_file_name
         sections = parse_nl_cli(args=args, nl_section_name=nl_section_name,
-            nl_info_dict=nl_info_dict)
+                                nl_info_dict=nl_info_dict)
         nl_sections[nl_section_name] = sections[nl_section_name]
 
     return nl_sections
