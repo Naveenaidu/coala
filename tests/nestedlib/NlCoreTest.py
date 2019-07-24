9bdca9924e3e2403c0907ab4ff5c00fb24a7daa7
Naveenaidu <naveennaidu479@gmail.com>
2019-07-24 18:17:13 +0530

    NlCoreTest: Add tests

---
+++ tests/nestedlib/NlCoreTest.py
@@ -0,0 +1,69 @@
+import unittest
+import logging
+
+from coalib.parsing.DefaultArgParser import default_arg_parser
+
+from coalib.nestedlib.NlCore import (get_parser,
+                                     get_nl_coala_sections,
+                                     nested_language)
+from coalib.nestedlib.parsers.PyJinjaParser import PyJinjaParser
+
+
+class NlCoreTest(unittest.TestCase):
+
+    def setUp(self):
+        self.arg_parser = default_arg_parser()
+        self.arg_list = ['--no-config', '--handle-nested',
+                         '--bears=PEP8Bear,Jinja2Bear',
+                         '--languages=python,jinja2', '--files=test.py',
+                         ]
+        self.args = self.arg_parser.parse_args(self.arg_list)
+
+    def test_get_parser_PyJinjaParser(self):
+        uut_lang_comb = 'python,jinja2'
+        parser = get_parser(uut_lang_comb)
+        assert isinstance(parser, PyJinjaParser)
+
+        # Test for parser not found
+        uut_lang_comb = 'python,cpp'
+        logger = logging.getLogger()
+        with self.assertLogs(logger, 'ERROR') as cm:
+            with self.assertRaises(SystemExit):
+                parser = get_parser(uut_lang_comb)
+                self.assertRegex(
+                    cm.output[0],
+                    "No Parser found for the languages combination")
+
+    def test_get_nl_coala_sections(self):
+
+        uut_nl_sections = get_nl_coala_sections(args=self.args)
+        self.assertEqual(
+            str(uut_nl_sections['cli_nl_section: test.py_nl_python']),
+            "cli_nl_section: test.py_nl_python {targets : '', " +
+            "bears : 'PEP8Bear', files : 'test.py_nl_python', " +
+            "handle_nested : 'True', languages : 'python,jinja2', " +
+            "no_config : 'True', file_lang : 'python', " +
+            "orig_file_name : 'test.py'}")
+
+        # When arg_list is passed
+        uut_nl_sections = get_nl_coala_sections(arg_list=self.arg_list)
+        self.assertEqual(
+            str(uut_nl_sections['cli_nl_section: test.py_nl_python']),
+            "cli_nl_section: test.py_nl_python {targets : '', " +
+            "bears : 'PEP8Bear', files : 'test.py_nl_python', " +
+            "handle_nested : 'True', languages : 'python,jinja2', " +
+            "no_config : 'True', file_lang : 'python', " +
+            "orig_file_name : 'test.py'}")
+
+    def test_nested_language(self):
+        # When --handle-nested is present
+        handle_nested = nested_language(args=self.args)
+        self.assertTrue(handle_nested)
+
+        # When --handle-nested is passed via arg_list
+        handle_nested = nested_language(arg_list=self.arg_list)
+        self.assertTrue(handle_nested)
+
+        # When --handle-nested is not present
+        handle_nested = nested_language(arg_list=[])
+        self.assertFalse(handle_nested)
